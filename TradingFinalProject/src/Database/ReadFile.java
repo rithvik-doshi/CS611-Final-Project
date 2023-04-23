ea972ee8dd2b03faa3db2cb4ed77ad4c7063b7fc
kevinge7 <yuxig5@bu.edu>
2023-04-23 19:24:51 -0400

    update some txt file and change Person.java to abstract class

---
+++ TradingFinalProject/src/Database/ReadFile.java
@@ -0,0 +1,14 @@
+package Database;
+
+import Model.Person;
+
+import java.util.ArrayList;
+
+public class ReadFile {
+    private Person person;
+    private ArrayList<Person> fileStream;
+
+    public void loadFile(){
+
+    }
+}
