931df72c6d6856add33803fd63d72db1546618ba
Zhangde <sozhangd@bu.edu>
2023-04-29 13:44:45 -0400

    View Sample

--- TradingFinalProject/src/Database/DBTest.java
+++ TradingFinalProject/src/Database/DBTest.java
@@ -1,47 +1,41 @@
-package Database;
+// package Database;
 
-import Model.Person;
-import Model.Request;
-import Model.RequestFactory;
-import Model.StockMarket;
+// import Model.Person;
+// import Model.StockMarket;
 
-import java.nio.file.Paths;
-import java.util.ArrayList;
+// import java.nio.file.Paths;
+// import java.util.ArrayList;
 
-public class DBTest {
-    public static void main(String[] args) {
-        //read file code
-        //name is before change is Rithvik Doshi
-        // String currentPath = Paths.get("").toAbsolutePath().toString();
-        // currentPath = currentPath + "/TradingFinalProject/src/Database/DBFiles/";
-        // ArrayList<Person> fileStream = CustomerFile.readFile(currentPath + "Customer.txt");
-        // for(int i = 0; i < fileStream.size();i++){
-        //     System.out.println("Name: " + fileStream.get(i).getName());
-        //     System.out.println("Email: " + fileStream.get(i).getEmail());
-        // }
+// public class DBTest {
+//     public static void main(String[] args) {
+//         //read file code
+//         //name is before change is Rithvik Doshi
+//         String currentPath = Paths.get("").toAbsolutePath().toString();
+//         currentPath = currentPath + "/TradingFinalProject/src/Database/DBFiles/";
+//         ArrayList<Person> fileStream = CustomerFile.readFile(currentPath + "Customer.txt");
+//         for(int i = 0; i < fileStream.size();i++){
+//             System.out.println("Name: " + fileStream.get(i).getName());
+//             System.out.println("Email: " + fileStream.get(i).getEmail());
+//         }
 
-        // //now make some changes and over-write the txt file
-        // fileStream.get(0).setName("Boston");
+//         //now make some changes and over-write the txt file
+//         fileStream.get(0).setName("Boston");
 
-        // CustomerFile.writeFile(currentPath + "Customer.txt", fileStream);
-        // //re-read file again to see the changes
-        // System.out.println("After changing, we get");
-        // fileStream = CustomerFile.readFile(currentPath + "Customer.txt");
-        // for (Person person : fileStream) {
-        //     System.out.println("Name: " + person.getName());
-        //     System.out.println("Email: " + person.getEmail());
-        // }
+//         CustomerFile.writeFile(currentPath + "Customer.txt", fileStream);
+//         //re-read file again to see the changes
+//         System.out.println("After changing, we get");
+//         fileStream = CustomerFile.readFile(currentPath + "Customer.txt");
+//         for (Person person : fileStream) {
+//             System.out.println("Name: " + person.getName());
+//             System.out.println("Email: " + person.getEmail());
+//         }
 
-        // testStocks();
+//         testStocks();
+//     }
 
-        RequestFactory requestFactory = new RequestFactory();
-        requestFactory.createRequests();
-        Request request = new Request("Rithvik");
-    }
+//     public static void testStocks(){
+//         StockMarket.instance.addStock("Apple", 100);
+//         StockMarket.instance.removeStock("Apple");
+//     }
 
-    public static void testStocks(){
-        StockMarket.instance.addStock("Apple", 100);
-        StockMarket.instance.removeStock("Apple");
-    }
-
-}
+// }
