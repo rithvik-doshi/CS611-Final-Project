8fb3c099bea368be09d9babc561ab6bfea6dc718
kevinge7 <yuxig5@bu.edu>
2023-05-03 17:38:00 -0400

    link few pages

--- TradingFinalProject/src/View/StockMarketView.java
+++ TradingFinalProject/src/View/StockMarketView.java
@@ -1,5 +1,8 @@
 package View;
 
+import Model.StockMarket;
+import Model.MarketStock;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -22,14 +25,12 @@ public class StockMarketView extends JFrame {
         setLocationRelativeTo(null);
 
         // Set up the table and scroll pane
-        String[] columnNames = {"Price", "Name", "ID", "Date"};
-        Object[][] data = {{"$10.00", "Apple Inc.", "AAPL", "2023-04-27"},
-                           {"$20.00", "Microsoft Corporation", "MSFT", "2023-04-27"},
-                           {"$30.00", "Amazon.com, Inc.", "AMZN", "2023-04-27"},
-                           {"$40.00", "Facebook, Inc.", "FB", "2023-04-27"}};
-        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        String[] columnNames = {"Price", "Name"};
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0); // Initially no data, just column names
         stockTable = new JTable(model);
         scrollPane = new JScrollPane(stockTable);
+
+        populateTableWithStocks(); // Populate the table with stock data
 
         // Set up the buttons
         backButton = new JButton("Back");
@@ -40,16 +41,27 @@ public class StockMarketView extends JFrame {
         backButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // TODO: Implement back button functionality
+                EntryInterface entryInterface = new EntryInterface();
+                StockMarketView.this.setVisible(false);
             }
         });
+
+        // Add action listeners to the buttons
         customerLoginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // TODO: Implement customer login button functionality
+                // Customer button clicked
+                LoginRegistrationPage customerLoginRegistrationPage = new LoginRegistrationPage();
+                StockMarketView.this.setVisible(false); // Set the current frame to be invisible
+
             }
         });
         managerLoginButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO: Implement manager login button functionality
+                // Customer button clicked
+                ManagerLoginPage managerLoginRegistrationPage = new ManagerLoginPage();
+                managerLoginRegistrationPage.setVisible(true);
+                StockMarketView.this.setVisible(false); // Set the current frame to be invisible
             }
         });
 
@@ -64,6 +76,14 @@ public class StockMarketView extends JFrame {
 
         // Add panel to the frame
         getContentPane().add(panel);
+    }
+
+    private void populateTableWithStocks() {
+        DefaultTableModel model = (DefaultTableModel) stockTable.getModel();
+        StockMarket stockMarket = StockMarket.instance;
+        for (MarketStock stock : stockMarket.stocks) {
+            model.addRow(new Object[]{String.format("$%.2f", stock.getMoney()), stock.getName(), "Unknown", "Unknown"});
+        }
     }
 
     public static void main(String[] args) {
