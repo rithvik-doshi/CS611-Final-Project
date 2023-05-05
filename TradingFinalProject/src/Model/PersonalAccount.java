3c092d9f8928b192084724a53306353236799dcb
kevinge7 <yuxig5@bu.edu>
2023-05-05 02:43:46 -0400

    update

--- TradingFinalProject/src/Model/PersonalAccount.java
+++ TradingFinalProject/src/Model/PersonalAccount.java
@@ -25,8 +25,8 @@ public class PersonalAccount extends Account {
 
 
     public static void updateCustomerFile(String filePath, int id, double newMoney) {
-        System.out.println(customerPath);
 
+        System.out.println("newMoney: " + newMoney);
         try {
             List<String> fileContent = new ArrayList<>(Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8));
             int lineNumber = -1;
@@ -42,6 +42,7 @@ public class PersonalAccount extends Account {
             if (lineNumber != -1) {
                 String[] fields = fileContent.get(lineNumber).split(", ");
                 fields[4] = String.valueOf(newMoney);
+                System.out.println("fields[4]"+fields[4]);
                 fileContent.set(lineNumber, String.join(", ", fields));
                 Files.write(Paths.get(filePath), fileContent, StandardCharsets.UTF_8);
                 System.out.println("Money updated successfully");
@@ -55,8 +56,21 @@ public class PersonalAccount extends Account {
 
 
     public boolean withdraw(double amount) {
-        if (this.balance >= amount) {
-            this.balance -= amount;
+        if (balance >= amount) {
+            balance -= amount;
+
+            String data = "Withdraw" + ", " + amount+"\n";
+
+            try {
+                FileWriter fw = new FileWriter(filePath, true);
+                fw.write(data);
+                fw.close();
+                System.out.println("Successfully wrote to the file.");
+            } catch (IOException e) {
+                System.out.println("An error occurred while writing request to DBfile.");
+                e.printStackTrace();
+            }
+            updateCustomerFile(customerPath,id,balance);
             return true;
         } else {
             return false;
